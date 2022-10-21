3b88ad8950a8b4322a79099b241e1879df60f51b
Fortunella <sherbakovdaniil6@gmail.com>
2022-10-21 22:08:37 +0300

    Изменено решение в 13 задачи. Добавлена функция

--- 13/Program.cs
+++ 13/Program.cs
@@ -1,12 +1,17 @@
-﻿Console.WriteLine("Введите число: ");
+﻿int Find3Int(int a)
+{
+    while (a / 1000 != 0)
+        a /= 10;
+    return a % 10;
+}
+
+Console.WriteLine("Введите число: ");
 
 int N = Convert.ToInt32(Console.ReadLine());
 
 if (N > 99)
 {
-    while (N / 1000 != 0)
-        N /= 10;
-    Console.WriteLine(N % 10);
+     Console.WriteLine(Find3Int(N));
 }
 else
     Console.WriteLine("Третьей цифры нет");
